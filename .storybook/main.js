59c76770a40c5e35c4c22a92da255dea213a245c
David Y. Cargatser <cargaster@gmail.com>
2021-08-24 09:44:09 +0300

    Config storysource addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -8,6 +9,18 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     '@storybook/addon-knobs',
-    '@storybook/addon-storysource'
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src/lib/components')], 
+        },
+        loaderOptions: {
+          injectStoryParameters: false,
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    }
   ]
 }
